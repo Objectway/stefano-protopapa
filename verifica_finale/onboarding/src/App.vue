658b2f3c520699d68d64e73dafdde519461ae69d
protostefano <protostefano@github.com>
2019-06-04 09:53:31 +0200

    header and footer style

--- verifica_finale/onboarding/src/App.vue
+++ verifica_finale/onboarding/src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
     <header>
-        <i class="fas fa-bars"></i>
+        <i class="fas fa-bars" @click="openMenu = true"></i>
+        <div> CONNECTUS </div>
+        <i class="fas fa-sign-in-alt"></i>
     </header>
 
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/prospect">Lista Utenti</router-link>
+    <div class="Nav" v-if="openMenu">
+      <ul>
+        <li><router-link class="Nav__link" to="/">Home</router-link></li>
+        <li><router-link class="Nav__link" to="/prospect">Lista Utenti</router-link></li>
+      </ul>
+      <span @click="openMenu = false"> X </span>
     </div>
+
     <router-view/>
+    <footer>
+      © 2018 ObjectWay S.p.A. — P.IVA IT07114250967 — Trademarks and brands are the property of their respective owners.
+    </footer>
   </div>
 </template>
 
@@ -18,6 +27,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 
 export default class App extends Vue {
+  @Prop() private openMenu: Boolean = false ;
 
   created(){
     this.axios.get(this.$store.getters.getInfoEndPoint)
@@ -36,11 +46,55 @@ export default class App extends Vue {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   header{
+    display:flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
     background-color: #005dad;
     color: #fff;
-    font-size: 40px;
+    font-size: 32px;
     padding: 10px;
+    div{
+      width: 100%;
+      margin-left: 12px;
+      font-size: 24px;
+      font-weight: bold;
+    }
+  }
+
+  .Nav{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    opacity: 0.7;
+    display: block;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    background-color: black;
+    color: #fff;
+    text-align: center;
+    padding: 24px;
+    li{
+      margin-bottom: 16px;
+    }
+    &__link{
+      color: #fff;
+      text-decoration: none;
+      font-weight: bold;
+      font-size: 24px;
+    }
+  }
+
+  footer{
+    padding: 10px;
+    background-color: #3d3d3d;
+    color: white;
+    text-align: center;
+    font-size: 12px;
+    font-weight: bold;
   }
 }
 </style>
